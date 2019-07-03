1607b1ce91c1db2d4f6dca4bfafed43cbf26482a
Zhiying Weng <jean.weng86@gmail.com>
2019-07-03 15:41:18 +0800

    Adjust indentation & comment two constructor

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -2,162 +2,162 @@
 
 public class LinkedListDeque<T> {
 
-		public class Node {
-	      private T _item;
-	      private Node _next;
-	      private Node _prev;
-	      public Node(T i, Node p, Node n) {
-	          _item = i;
-	          _prev = p;
-	          _next = n;
-	      }
-	      public T getItem() {
-					return _item;
-				}
+	private class Node {
+	  private T _item;
+	  private Node _next;
+	  private Node _prev;
+	  public Node(T i, Node p, Node n) {
+	    _item = i;
+	    _prev = p;
+	    _next = n;
 	  }
-
-	  private Node sentinel;
-	  private int size = 0;
-
-		public LinkedListDeque() {
-			  sentinel = new Node(null, null, null);
-			  sentinel._next = sentinel;
-			  sentinel._prev = sentinel;
-		    size = 0;
-	  }
-
-		public LinkedListDeque(T x) {
-			  sentinel = new Node(null, null, null);
-			  Node node = new Node(x, sentinel, sentinel);
-			  sentinel._next = node;
-			  sentinel._prev = node;
-		   	size = 1;
+	  public T getItem() {
+			return _item;
 		}
+	}
 
-		/*
-		 * Creates a deep copy of other
-		 */
-	  public LinkedListDeque(LinkedListDeque other) {
-			  sentinel = new Node(null, null, null);
-			  sentinel._next = sentinel;
-			  sentinel._prev = sentinel;
-		   	size = 0;
+  private Node sentinel;
+  private int size = 0;
 
-			  for(int i = 0; i < other.size(); i++) {
-		   			addLast((T) other.get(i));
-			  }
+	public LinkedListDeque() {
+	  sentinel = new Node(null, null, null);
+	  sentinel._next = sentinel;
+	  sentinel._prev = sentinel;
+    size = 0;
+  }
+
+	// public LinkedListDeque(T x) {
+	// 	  sentinel = new Node(null, null, null);
+	// 	  Node node = new Node(x, sentinel, sentinel);
+	// 	  sentinel._next = node;
+	// 	  sentinel._prev = node;
+	//    	size = 1;
+	// }
+	//
+	// /*
+	//  * Creates a deep copy of other
+	//  */
+  // public LinkedListDeque(LinkedListDeque other) {
+	// 	  sentinel = new Node(null, null, null);
+	// 	  sentinel._next = sentinel;
+	// 	  sentinel._prev = sentinel;
+	//    	size = 0;
+	//
+	// 	  for(int i = 0; i < other.size(); i++) {
+	//    			addLast((T) other.get(i));
+	// 	  }
+  // }
+
+	/*
+	 * Adds an item of type T to the front of the deque
+	 */
+  public void addFirst(T item) {
+	  Node node = new Node(item, sentinel, sentinel._next);
+	  sentinel._next = node;
+	  node._next._prev = node;
+   	size++;
+  }
+
+	/*
+	 * Adds an item of type T to the back of the deque
+	 */
+  public void addLast(T item) {
+	  Node node = new Node(item, sentinel._prev, sentinel);
+	  sentinel._prev = node;
+	  node._prev._next = node;
+   	size++;
+  }
+
+	/*
+	 * Returns true if deque is empty, false otherwise
+	 */
+  public boolean isEmpty() {
+   	return size == 0;
+  }
+
+	/*
+	 * Returns the number of items in the deque
+	 */
+  public int size() {
+	  return size;
+  }
+
+	/*
+	 * Prints the items in the deque from first to last
+	 */
+  public void printDeque() {
+	  Node p = sentinel._next;
+	  while(p != null && p._item != null) {
+		  System.out.print(p._item + " ");
+		  p = p._next;
 	  }
+	  System.out.println();
+   }
 
-		/*
-		 * Adds an item of type T to the front of the deque
-		 */
-	  public void addFirst(T item) {
-			  Node node = new Node(item, sentinel, sentinel._next);
-			  sentinel._next = node;
-			  node._next._prev = node;
-		   	size++;
+	/*
+	 * Removes and returns the item at the front of the deque. If no such item
+	 * exists, returns null
+	 */
+  public T removeFirst() {
+	  if(size == 0) {
+			return null;
+		}
+	  Node removed = sentinel._next;
+	  sentinel._next = sentinel._next._next;
+	  sentinel._next._prev = sentinel;
+	  size--;
+	  return removed.getItem();
+  }
+
+	/*
+	 * Removes and returns the item at the back of the deque. If no such item
+	 * exists, returns null
+	 */
+  public T removeLast() {
+	  if(size == 0) {
+			return null;
+		}
+	  Node removed = sentinel._prev;
+	  sentinel._prev = sentinel._prev._prev;
+	  sentinel._prev._next = sentinel;
+	  size--;
+	  return removed.getItem();
+  }
+
+	/*
+	 * Gets the item at the given index using iteration. If no such item exists,
+	 * returns null
+	 */
+  public T get(int index) {
+	  if (index > size) {
+			return null;
+		}
+	  Node p = sentinel._next;
+	  while (index > 0) {
+		  p = p._next;
+		  index--;
 	  }
+	  return p.getItem();
+  }
 
-		/*
-		 * Adds an item of type T to the back of the deque
-		 */
-	  public void addLast(T item) {
-			  Node node = new Node(item, sentinel._prev, sentinel);
-			  sentinel._prev = node;
-			  node._prev._next = node;
-		   	size++;
+	/*
+	 * Gets the item at the given index using recursion. If no such item exists,
+	 * returns null
+	 */
+  public T getRecursive(int index) {
+	  if (index > size) {
+		  return null;
 	  }
+	  return getRecursive(index, sentinel._next);
+  }
 
-		/*
-		 * Returns true if deque is empty, false otherwise
-		 */
-	  public boolean isEmpty() {
-	   		return size == 0;
+	/*
+	 * Helper function for getRecursive(int index).
+	 */
+  private T getRecursive(int index, Node n) {
+	  if (index == 0) {
+		  return n.getItem();
 	  }
-
-		/*
-		 * Returns the number of items in the deque
-		 */
-	  public int size() {
-		  	return size;
-	  }
-
-		/*
-		 * Prints the items in the deque from first to last
-		 */
-	  public void printDeque() {
-			  Node p = sentinel._next;
-			  while(p != null && p._item != null) {
-					  System.out.print(p._item + " ");
-					  p = p._next;
-			  }
-			  System.out.println();
-	   }
-
-		/*
-		 * Removes and returns the item at the front of the deque. If no such item
-		 * exists, returns null
-		 */
-	  public T removeFirst() {
-			  if(size == 0) {
-						return null;
-				}
-			  Node removed = sentinel._next;
-			  sentinel._next = sentinel._next._next;
-			  sentinel._next._prev = sentinel;
-			  size--;
-			  return removed.getItem();
-	  }
-
-		/*
-		 * Removes and returns the item at the back of the deque. If no such item
-		 * exists, returns null
-		 */
-	  public T removeLast() {
-			  if(size == 0) {
-						return null;
-				}
-			  Node removed = sentinel._prev;
-			  sentinel._prev = sentinel._prev._prev;
-			  sentinel._prev._next = sentinel;
-			  size--;
-			  return removed.getItem();
-	  }
-
-		/*
-		 * Gets the item at the given index using iteration. If no such item exists,
-		 * returns null
-		 */
-	  public T get(int index) {
-			  if (index > size) {
-						return null;
-				}
-			  Node p = sentinel._next;
-			  while (index > 0) {
-					  p = p._next;
-					  index--;
-			  }
-			  return p.getItem();
-	  }
-
-		/*
-		 * Gets the item at the given index using recursion. If no such item exists,
-		 * returns null
-		 */
-	  public T getRecursive(int index) {
-			  if (index > size) {
-				  	return null;
-			  }
-			  return getRecursive(index, sentinel._next);
-	  }
-
-		/*
-		 * Helper function for getRecursive(int index).
-		 */
-	  private T getRecursive(int index, Node n) {
-			  if (index == 0) {
-				  	return n.getItem();
-			  }
-			  return getRecursive(index - 1, n._next);
-	  }
+	  return getRecursive(index - 1, n._next);
+  }
 }
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,6 +1,5 @@
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
-	
 	/* Utility method for printing out empty checks. */
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
@@ -19,7 +18,7 @@ public class LinkedListDequeTest {
 		return true;
 	}
 
-	/* Prints a nice message based on whether a test passed. 
+	/* Prints a nice message based on whether a test passed.
 	 * The \n means newline. */
 	public static void printTestStatus(boolean passed) {
 		if (passed) {
@@ -29,20 +28,20 @@ public class LinkedListDequeTest {
 		}
 	}
 
-	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
-	  * finally printing the results. 
+	/** Adds a few things to the list, checking isEmpty() and size() are correct,
+	  * finally printing the results.
 	  *
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
-		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		// System.out.println("Running add/isEmpty/Size test.");
+		// System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
 		/*
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst("front");
-		
+
 		// The && operator is the same as "and" in Python.
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
 		passed = checkSize(1, lld1.size()) && passed;
@@ -64,20 +63,19 @@ public class LinkedListDequeTest {
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
 	public static void addRemoveTest() {
 
-		System.out.println("Running add/remove test.");
-
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		// System.out.println("Running add/remove test.");
+		// System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
 		/*
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty 
+		// should be empty
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst(10);
-		// should not be empty 
+		// should not be empty
 		passed = checkEmpty(false, lld1.isEmpty()) && passed;
 
 		lld1.removeFirst();
-		// should be empty 
+		// should be empty
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
@@ -89,4 +87,4 @@ public class LinkedListDequeTest {
 		addIsEmptySizeTest();
 		addRemoveTest();
 	}
-} 
+}
