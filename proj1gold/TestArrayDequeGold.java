738c98a9c2e75dd7246b92fade7f864181143eaa
Zhiying Weng <jean.weng86@gmail.com>
2019-07-24 13:57:57 +0800

    Update failure case in TestArrayDequeGold.java

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,13 +7,10 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         sad1.addFirst(5);
         sad1.addFirst(3);
-        sad1.printDeque();
         if(!sad1.isEmpty()){
             Integer s1 = sad1.removeFirst();
 
-            assertEquals("\naddFirst(5)\naddFirst(3)\nremoveFirst() ", s1, (Integer)3);
-            Integer s2 = sad1.removeLast();
-            assertEquals("\naddFirst(5)\naddFirst(3)\nremoveFirst()\nremoveLast() ", s2, (Integer)3);
+            assertEquals("\naddFirst(5)\naddFirst(3)\nremoveFirst() ", s1, (Integer)5);
 
         }
 
