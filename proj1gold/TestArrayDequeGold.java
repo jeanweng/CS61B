35a526272731fbf3744cc8bdc52418e0f93d37b1
Zhiying Weng <jean.weng86@gmail.com>
2019-07-24 13:22:59 +0800

    Add TestArrayDequeGold.java

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void test1() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        sad1.addFirst(5);
+        sad1.addFirst(3);
+        sad1.printDeque();
+        if(!sad1.isEmpty()){
+            Integer s1 = sad1.removeFirst();
+
+            assertEquals("\naddFirst(5)\naddFirst(3)\nremoveFirst() ", s1, (Integer)3);
+        }
+
+    }
+}
