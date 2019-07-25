5d5f1e250f935943d391023ba289c19ce09f73a6
Zhiying Weng <jean.weng86@gmail.com>
2019-07-25 11:17:53 +0800

    Update proj1Gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -4,15 +4,38 @@ import org.junit.Test;
 public class TestArrayDequeGold {
     @Test
     public void test1() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        sad1.addFirst(5);
-        sad1.addFirst(3);
-        if(!sad1.isEmpty()){
-            Integer s1 = sad1.removeFirst();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StringBuilder sb = new StringBuilder();
+        int size = 0;
+        Integer resSad = null;
+        Integer resAds = null;
 
-            assertEquals("\naddFirst(5)\naddFirst(3)\nremoveFirst() ", s1, (Integer)5);
-
+        while(true){
+            double random = StdRandom.uniform();
+            int num = (int)(random * 10);
+            if (random < 0.25) {
+                sad.addLast(num);
+                ads.addLast(num);
+                size++;
+                sb.append("addLast(" + num + ")\n");
+            } else if(random < 0.5){
+                sad.addFirst(num);
+                ads.addFirst(num);
+                size++;
+                sb.append("addFirst(" + num + ")\n");
+            } else if(random < 0.75 && size > 0){
+                resSad = sad.removeFirst();
+                resAds = ads.removeFirst();
+                size--;
+                sb.append("removeFirst()\n");
+            } else if(size > 0) {
+                resSad = sad.removeLast();
+                resAds = ads.removeLast();
+                size--;
+                sb.append("removeLast()\n");
+            }
+            assertEquals(sb.toString(), resAds, resSad);
         }
-
     }
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,22 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    public void addHexagon(int length, int x, int y){
+        for(int i = x; i < x + length; i++){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        HexWorld hw = new HexWorld();
+
+        TETile[][] HexTiles = new TETile[WIDTH][HEIGHT];
+        hw.addHexagon(2, 10,10);
+        ter.renderFrame(HexTiles);
+    }
 }
